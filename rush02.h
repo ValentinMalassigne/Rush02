1540f21c6ae84aad73d6ff04a1099e17603c0b33
Lionel Guyon <liguyon@c2r12p4.42lehavre.fr>
2023-03-18 12:39:37 +0100

    add struct definition

--- rush02.h
+++ rush02.h
@@ -6,11 +6,17 @@
 /*   By: liguyon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 12:28:04 by liguyon           #+#    #+#             */
-/*   Updated: 2023/03/18 12:28:52 by liguyon          ###   ########.fr       */
+/*   Updated: 2023/03/18 12:38:16 by liguyon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RUSH02_H
 # define RUSH02_H
+
+typedef struct s_number_dict
+{
+	int	number;
+	char	*word_en;
+}	t_number_dict;
 
 #endif
